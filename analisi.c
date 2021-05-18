a29e8d998b82887965025e7167d627776dea2ebb
FrnkDarko <frnkdarko@users.noreply.github.com>
2021-05-18 15:21:01 +0200

    L'analisi dell'ampiezza funziona
    la fase ancora no

--- analisi.c
+++ analisi.c
@@ -8,9 +8,9 @@
 void analisi()
 {
     TGraph *freq = new TGraph("frequenza_sweep.txt");
-    TF1 *f1 = new TF1("f1", "((x^2) * ([0]^2)) / sqrt(((((x^2) * ([0]^2)) - ([1]^2))^2) + ((3 * x * [0] * [1])^2))", 500, 49000);
-    f1->SetParameter(0, 0.02157); //L
-    f1->SetParameter(1, 1795.5); //R
+    TF1 *f1 = new TF1("f1", "(((x*2*TMath::Pi())^2) * ([0]^2)) / sqrt((((((x*2*TMath::Pi())^2) * ([0]^2)) - ([1]^2))^2) + ((3 * (x*2*TMath::Pi()) * [0] * [1])^2))*5");
+    f1->SetParameter(0, 0.04673); //L
+    f1->SetParameter(1, 3290); //R
     TCanvas *c1 = new TCanvas("c1", "Analisi ampiezza");
     gStyle->SetOptFit(0111);
     freq->Fit("f1");
@@ -19,9 +19,9 @@ void analisi()
     
 
     TGraph *fase = new TGraph("fase_sweep.txt");
-    TF1 *f2 = new TF1("f2", "TMath::ATan((3 * x * [0] * [1]) / (((x^2) * ([0]^2)) - ([1]^2)))", 500, 49000);
-    f2->SetParameter(0, 0.02157);
-    f2->SetParameter(1, 1795.5); 
+    TF1 *f2 = new TF1("f2", "TMath::ATan2((3 * (x*2*TMath::Pi()) * [0] * [1] * (5*TMath::Pi()/180)), ((((x*2*TMath::Pi())^2) * ([0]^2)) - ([1]^2)))");
+    f2->SetParameter(0, 0.04673);
+    f2->SetParameter(1, 3290); 
     TCanvas *c2 = new TCanvas("c2", "Analisi fase");
     gStyle->SetOptFit(0111);
     fase->Fit("f2");
